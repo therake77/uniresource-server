18774b62fdf3d822a19f3e06a8f17b0323af7e66
josemd1412 <jemd2203@gmail.com>
2025-12-13 22:50:38 -0500

    UPD: usando rutas

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 import Login from './pages/Login';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <Login />
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
